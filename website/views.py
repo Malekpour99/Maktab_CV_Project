2567f6c5d5a8da95e12aea657b7068ef83410ae2
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-11-16 14:18:52 +0330

    website index_view
    Created resume object for injecting this dynamic data to the index template

--- website/views.py
+++ website/views.py
@@ -2,5 +2,48 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index_view(request):
-    return render(request, "website\index.html")
+    resume = {
+        "area_skills": [
+            {
+                "area": "Front-End",
+                "skill_level": 98
+            },
+            {
+                "area": "Back-End",
+                "skill_level": 76
+            },
+            {
+                "area": "Databases",
+                "skill_level": 63
+            },
+        ],
+        "programming_skills": [
+            {
+                "language": "Python",
+                "skill_level": 96
+            },
+            {
+                "language": "Vue-JS / Vuex / VueRouter",
+                "skill_level": 90
+            },
+            {
+                "language": "Django",
+                "skill_level": 70
+            },
+            {
+                "language": "JavaScript / TypeScript",
+                "skill_level": 86
+            },
+            {
+                "language": "HTML5",
+                "skill_level": 100
+            },
+            {
+                "language": "CSS / SCSS / SASS",
+                "skill_level": 94
+            },
+        ],
+    }
+    return render(request, "website\index.html", {"resume": resume})
